734c6cbf237d58bc30fab56be60516ed03499a0d
Danny Arnold <danny@kietsy.com>
2015-01-24 22:06:00 +0100

    change dimensions

--- src/js/boot.js
+++ src/js/boot.js
@@ -14,12 +14,14 @@
 
       if (this.game.device.desktop) {
         this.game.scale.pageAlignHorizontally = true;
+        this.game.scale.width = 1024
+        this.game.scale.height = 768
       } else {
         this.game.scaleMode                   = Phaser.ScaleManager.SHOW_ALL;
         // this.game.scale.minWidth              = 480;
         // this.game.scale.minHeight             = 260;
-        this.game.scale.maxWidth              = 800;
-        this.game.scale.maxHeight             = 640;
+        this.game.scale.maxWidth              = 1024;
+        this.game.scale.maxHeight             = 768;
         this.game.scale.forceLandscape        = true;
         this.game.scale.pageAlignHorizontally = true;
         this.game.scale.setScreenSize(true);
